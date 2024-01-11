acbaf30ccef139654e075a44696c48c8cc5d030a
trillnepill <97785291+trillnepill@users.noreply.github.com>
2024-01-11 12:21:02 +0000

    test commit?

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,19 +5,11 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: "PWA with Next 13",
-  description: "PWA application with Next 13",
+  title: "Однажды в сказке",
+  description: "Веб-портал с сказками, написанными нейросетью, и иллюстрациями для детей любого возраста. Читай, учись, пиши!",
   generator: "Next.js",
   manifest: "/manifest.json",
-  keywords: ["nextjs", "nextjs13", "next13", "pwa", "next-pwa"],
-  themeColor: [{ media: "(prefers-color-scheme: dark)", color: "#fff" }],
-  authors: [
-    { name: "Rajesh Prajapati" },
-    {
-      name: "Rajesh Prajapati",
-      url: "https://www.linkedin.com/in/raazeshp96/",
-    },
-  ],
+  keyboards: ["сказки", "истории", "tales", "однаждывсказке", "onceuponatale"],
   viewport:
     "minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover",
   icons: [
@@ -32,7 +24,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="ru">
       <body className={inter.className}>{children}</body>
     </html>
   )
