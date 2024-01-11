19a7bf587bf884b37a7561d40a39095b86ee42d6
Nikita <banana@banana.is-cool.dev>
2024-01-11 15:37:31 +0300

    move navbar to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import { cn } from '@/lib/utils';
+import { Button } from '@/components/ui/button';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -26,7 +27,24 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ru">
-      <body className={cn(inter.className, "container")}>{children}</body>
+      <body className={cn(inter.className, "container")}>
+           <main className="flex min-h-screen flex-col justify-between p-16 lg:p-24 items-center">
+      
+      <div className="z-10 w-full items-center justify-between text-sm lg:flex">
+        <h1 className="font-bold text-2xl">Однажды в сказке</h1>
+        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+          <a
+            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
+            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Button className="bg-amber-500">Предложить сказку</Button>
+          </a>
+        </div>
+      </div>{children}
+      </main>
+      </body>
     </html>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -11,21 +11,7 @@ import {
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col justify-between p-16 lg:p-24 items-center">
-      
-      <div className="z-10 w-full items-center justify-between text-sm lg:flex">
-        <h1 className="font-bold text-2xl">Однажды в сказке</h1>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Button className="bg-amber-500">Предложить сказку</Button>
-          </a>
-        </div>
-      </div>
+ 
 
       <Carousel
         opts={{
@@ -49,6 +35,5 @@ export default function Home() {
         <CarouselPrevious />
         <CarouselNext />
       </Carousel>
-    </main>
   )
 }
